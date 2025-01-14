15739674f1cca0207e03909d07c15a75150a02c4
kimoyerr <kimoyerr@gmail.com>
2025-01-14 08:51:26 +0000

    Comma space

--- provarun/models.py
+++ provarun/models.py
@@ -146,7 +146,7 @@ class GPT(nn.Module):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
         
 
-    def forward(self, x , labels=None):
+    def forward(self, x, labels=None):
         batch_size, seq_len = x.size()
         current_idx = 0  # TODO: Implement sliding window for long sequences
         h = self.wte(x)  # Shape [batch_size, seq_len, dim]
