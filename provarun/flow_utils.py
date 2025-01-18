a7824882c3a0913a38bfb66d8a55a7deb9d8cdef
kimoyerr <kimoyerr@gmail.com>
2025-01-18 23:02:59 +0000

    Added code for flow-matching based on meta's code

---
+++ provarun/flow_utils.py
@@ -0,0 +1,241 @@
+from typing import Union
+from dataclasses import dataclass, field
+import torch
+from torch import nn
+
+
+@dataclass
+class SchedulerOutput:
+    r"""Represents a sample of a conditional-flow generated probability path.
+
+    Attributes:
+        alpha_t (Tensor): :math:`\alpha_t`, shape (...).
+        sigma_t (Tensor): :math:`\sigma_t`, shape (...).
+        d_alpha_t (Tensor): :math:`\frac{\partial}{\partial t}\alpha_t`, shape (...).
+        d_sigma_t (Tensor): :math:`\frac{\partial}{\partial t}\sigma_t`, shape (...).
+
+    """
+
+    alpha_t: torch.Tensor = field(metadata={"help": "alpha_t"})
+    sigma_t: torch.Tensor = field(metadata={"help": "sigma_t"})
+    d_alpha_t: torch.Tensor = field(metadata={"help": "Derivative of alpha_t."})
+    d_sigma_t: torch.Tensor = field(metadata={"help": "Derivative of sigma_t."})
+
+
+
+# Modified from https://github.com/facebookresearch/flow_matching/blob/main/flow_matching/path/path_sample.py
+@dataclass
+class DiscretePathSample:
+    """
+    Represents a sample of a conditional-flow generated discrete probability path.
+
+    Attributes:
+        x_1 (Tensor): the target sample :math:`X_1`.
+        x_0 (Tensor): the source sample :math:`X_0`.
+        t (Tensor): the time sample  :math:`t`.
+        x_t (Tensor): the sample along the path  :math:`X_t \sim p_t`.
+    """
+
+    x_1: torch.Tensor = field(metadata={"help": "target samples X_1 (batch_size, ...)."})
+    x_0: torch.Tensor = field(metadata={"help": "source samples X_0 (batch_size, ...)."})
+    t: torch.Tensor = field(metadata={"help": "time samples t (batch_size, ...)."})
+    x_t: torch.Tensor = field(
+        metadata={"help": "samples X_t ~ p_t(X_t), shape (batch_size, ...)."}
+    )
+
+
+# MaskedSourceDistribution
+class MaskedSourceDistribution():
+    def __init__(self, mask_token: int) -> None:
+        self.mask_token = mask_token
+
+    @property
+    def masked(self) -> bool:
+        return True
+
+    def sample(self, tensor_size, device):
+        return torch.zeros(tensor_size, device=device).fill_(self.mask_token).long()
+
+    def sample_like(self, tensor_like):
+        return torch.zeros_like(tensor_like).fill_(self.mask_token).long()
+    
+
+# Modified from: https://github.com/facebookresearch/flow_matching/blob/main/flow_matching/path/scheduler/scheduler.py
+class PolynomialConvexScheduler():
+    """Polynomial Scheduler."""
+
+    def __init__(self, n: Union[float, int]):
+        assert isinstance(
+            n, (float, int)
+        ), f"`n` must be a float or int. Got {type(n)=}."
+        assert n > 0, f"`n` must be positive. Got {n=}."
+
+        self.n = n
+
+    def __call__(self, t: torch.Tensor):
+        return SchedulerOutput(
+            alpha_t=t**self.n,
+            sigma_t=1 - t**self.n,
+            d_alpha_t=self.n * (t ** (self.n - 1)),
+            d_sigma_t=-self.n * (t ** (self.n - 1)),
+        )
+
+    def kappa_inverse(self, kappa: torch.Tensor):
+        return torch.pow(kappa, 1.0 / self.n)
+    
+
+
+# Modified from: https://github.com/facebookresearch/flow_matching/blob/main/flow_matching/utils/utils.py
+def expand_tensor_like(input_tensor, expand_to):
+    """`input_tensor` is a 1d vector of length equal to the batch size of `expand_to`,
+    expand `input_tensor` to have the same shape as `expand_to` along all remaining dimensions.
+
+    Args:
+        input_tensor (Tensor): (batch_size,).
+        expand_to (Tensor): (batch_size, ...).
+
+    Returns:
+        Tensor: (batch_size, ...).
+    """
+    assert input_tensor.ndim == 1, "Input tensor must be a 1d vector."
+    assert (
+        input_tensor.shape[0] == expand_to.shape[0]
+    ), f"The first (batch_size) dimension must match. Got shape {input_tensor.shape} and {expand_to.shape}."
+
+    dim_diff = expand_to.ndim - input_tensor.ndim
+
+    t_expanded = input_tensor.clone()
+    t_expanded = t_expanded.reshape(-1, *([1] * dim_diff))
+
+    return t_expanded.expand_as(expand_to)
+
+
+# Modified from: https://github.com/facebookresearch/flow_matching/blob/main/flow_matching/utils/utils.py
+def unsqueeze_to_match(source, target, how="suffix"):
+    """
+    Unsqueeze the source tensor to match the dimensionality of the target tensor.
+
+    Args:
+        source (Tensor): The source tensor to be unsqueezed.
+        target (Tensor): The target tensor to match the dimensionality of.
+        how (str, optional): Whether to unsqueeze the source tensor at the beginning
+            ("prefix") or end ("suffix"). Defaults to "suffix".
+
+    Returns:
+        Tensor: The unsqueezed source tensor.
+    """
+    assert (
+        how == "prefix" or how == "suffix"
+    ), f"{how} is not supported, only 'prefix' and 'suffix' are supported."
+
+    dim_diff = target.dim() - source.dim()
+
+    for _ in range(dim_diff):
+        if how == "prefix":
+            source = source.unsqueeze(0)
+        elif how == "suffix":
+            source = source.unsqueeze(-1)
+
+    return source
+
+
+# Modified from https://github.com/facebookresearch/flow_matching/blob/main/flow_matching/path/mixture.py
+class MixtureDiscreteProbPath():
+    r"""The ``MixtureDiscreteProbPath`` class defines a factorized discrete probability path.
+
+    This path remains constant at the source data point :math:`X_0` until a random time, determined by the scheduler, when it flips to the target data point :math:`X_1`.
+    The scheduler determines the flip probability using the parameter :math:`\sigma_t`, which is a function of time `t`. Specifically, :math:`\sigma_t` represents the probability of remaining at :math:`X_0`, while :math:`1 - \sigma_t` is the probability of flipping to :math:`X_1`:
+
+    .. math::
+
+        P(X_t = X_0) = \sigma_t \quad \text{and} \quad  P(X_t = X_1) = 1 - \sigma_t,
+
+    where :math:`\sigma_t` is provided by the scheduler.
+
+    Example:
+
+    .. code-block:: python
+
+        >>> x_0 = torch.zeros((1, 3, 3))
+        >>> x_1 = torch.ones((1, 3, 3))
+
+        >>> path = MixtureDiscreteProbPath(PolynomialConvexScheduler(n=1.0))
+        >>> result = path.sample(x_0, x_1, t=torch.tensor([0.1])).x_t
+        >>> result
+        tensor([[[0.0, 0.0, 0.0],
+                 [0.0, 0.0, 1.0],
+                 [0.0, 0.0, 0.0]]])
+
+        >>> result = path.sample(x_0, x_1, t=torch.tensor([0.5])).x_t
+        >>> result
+        tensor([[[1.0, 0.0, 1.0],
+                 [0.0, 1.0, 0.0],
+                 [0.0, 1.0, 0.0]]])
+
+        >>> result = path.sample(x_0, x_1, t=torch.tensor([1.0])).x_t
+        >>> result
+        tensor([[[1.0, 1.0, 1.0],
+                 [1.0, 1.0, 1.0],
+                 [1.0, 1.0, 1.0]]])
+
+    Args:
+        scheduler (ConvexScheduler): The scheduler that provides :math:`\sigma_t`.
+    """
+
+    def __init__(self, scheduler):
+        assert isinstance(
+            scheduler, PolynomialConvexScheduler
+        ), "Scheduler for ConvexProbPath must be a ConvexScheduler."
+
+        self.scheduler = scheduler
+
+    def sample(self, x_0, x_1, t):
+        r"""Sample from the affine probability path:
+            | given :math:`(X_0,X_1) \sim \pi(X_0,X_1)` and a scheduler :math:`(\alpha_t,\sigma_t)`.
+            | return :math:`X_0, X_1, t`, and :math:`X_t \sim p_t`.
+        Args:
+            x_0 (Tensor): source data point, shape (batch_size, ...).
+            x_1 (Tensor): target data point, shape (batch_size, ...).
+            t (Tensor): times in [0,1], shape (batch_size).
+
+        Returns:
+            DiscretePathSample: a conditional sample at :math:`X_t ~ p_t`.
+        """
+        self.assert_sample_shape(x_0=x_0, x_1=x_1, t=t)
+
+        sigma_t = self.scheduler(t).sigma_t
+
+        sigma_t = expand_tensor_like(input_tensor=sigma_t, expand_to=x_1)
+
+        source_indices = torch.rand(size=x_1.shape, device=x_1.device) < sigma_t
+        x_t = torch.where(condition=source_indices, input=x_0, other=x_1)
+
+        return DiscretePathSample(x_t=x_t, x_1=x_1, x_0=x_0, t=t)
+
+    def posterior_to_velocity(
+        self, posterior_logits, x_t, t
+    ):
+        r"""Convert the factorized posterior to velocity.
+
+        | given :math:`p(X_1|X_t)`. In the factorized case: :math:`\prod_i p(X_1^i | X_t)`.
+        | return :math:`u_t`.
+
+        Args:
+            posterior_logits (Tensor): logits of the x_1 posterior conditional on x_t, shape (..., vocab size).
+            x_t (Tensor): path sample at time t, shape (...).
+            t (Tensor): time in [0,1].
+
+        Returns:
+            Tensor: velocity.
+        """
+        posterior = torch.softmax(posterior_logits, dim=-1)
+        vocabulary_size = posterior.shape[-1]
+        x_t = nn.functional.one_hot(x_t, num_classes=vocabulary_size)
+        t = unsqueeze_to_match(source=t, target=x_t)
+
+        scheduler_output = self.scheduler(t)
+
+        kappa_t = scheduler_output.alpha_t
+        d_kappa_t = scheduler_output.d_alpha_t
+
+        return (d_kappa_t / (1 - kappa_t)) * (posterior - x_t)
