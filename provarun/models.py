54f2e16b61b87370915488e0d1d31246547bd812
kimoyerr <kimoyerr@gmail.com>
2025-01-19 18:56:32 +0000

    Deal with timesteps shape with 1 in second dimension

--- provarun/models.py
+++ provarun/models.py
@@ -14,12 +14,14 @@ def transformer_timestep_embedding(timesteps, embedding_dim, max_positions=10000
     # assumes timesteps is in the range 0 to 1000
 
     assert len(timesteps.shape) == 1  or timesteps.shape[1] == 1
-    timesteps.squeeze_(1)
+    if len(timesteps.shape) > 1:
+        timesteps.squeeze_(1)
     half_dim = embedding_dim // 2
     # magic number 10000 is from transformers
     emb = math.log(max_positions) / (half_dim - 1)
     # emb = math.log(2.) / (half_dim - 1)
-    emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
+    emb = torch.exp(torch.arange(half_dim, dtype=torch.float32) * -emb)
+    emb = emb.to(timesteps.device)
     # emb = tf.range(num_embeddings, dtype=jnp.float32)[:, None] * emb[None, :]
     # emb = tf.cast(timesteps, dtype=jnp.float32)[:, None] * emb[None, :]
     emb = timesteps.float()[:, None] * emb[None, :]
